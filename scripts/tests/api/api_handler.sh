cdcd934c737a4784b2998b7383ca38422c89e818
gorobey <giulio@gorobey.it>
2024-11-28 00:59:51 +0100

    rimosso include inutile

--- scripts/tests/api/api_handler.sh
+++ scripts/tests/api/api_handler.sh
@@ -2,9 +2,6 @@
 
 #usage: echo '{"command": "get_now_playing"}' | socat - TCP:localhost:12345
 
-# Import logging helper
-source ../../../helpers/logging.sh
-
 # Leggi la richiesta dalla connessione socket
 read request
 
